d64a79004970f497e7ccb4e5b053caefb26ab0da
Professor Aílson <102998807+Prof-Ailson@users.noreply.github.com>
2025-08-08 07:36:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ function inserirNome (){
     elemento.textContent = nomeUsuario;
 }
 
+
 inserirNome();
+
+let disciplinass = ["Português", "Matemática","Física", "Química"];
+
+const item = document.querySelector("#lista");
+item.textContent = linguagens[0] + ' - ' +  linguagens[1] + ' - ' +  linguagens[2] + ' - ' +  linguagens[3];
+//item.textContent = linguagens; 
